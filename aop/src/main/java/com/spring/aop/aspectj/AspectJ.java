96bfe68ecc2d223d05b49662268a9cbfef95edc5
rkritchat <rkritchat@SCAN-IT-THBKK28>
2018-03-15 11:02:15 +0700

    Create Class black color

--- aop/src/main/java/com/spring/aop/aspectj/AspectJ.java
+++ aop/src/main/java/com/spring/aop/aspectj/AspectJ.java
@@ -1,6 +1,10 @@
 package com.spring.aop.aspectj;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
@@ -10,5 +14,25 @@ public class AspectJ {
 	@Before("execution(* *(..))")
 	public void before(JoinPoint joinPoint) {
 		System.out.println("Before run business -->>");
+	}
+	
+	@After("execution(* *(..))")
+	public void after() {
+		
+	}
+	
+	@Around("execution(* *(..))")
+	public void around() {
+		
+	}
+	
+	@AfterReturning("execution(* *(..))")
+	public void afterReturning() {
+		
+	}
+	
+	@AfterThrowing("execution(* *(..))")
+	public void afterThrowing() {
+		
 	}
 }
---
+++ aop/src/main/java/com/spring/aop/color/Black.java
@@ -0,0 +1,12 @@
+package com.spring.aop.color;
+
+public class Black {
+	
+	public Black() {
+		System.out.println("Initial Balck class");
+	}
+	
+	public void showColor() {
+		System.out.println("Black");
+	}
+}
