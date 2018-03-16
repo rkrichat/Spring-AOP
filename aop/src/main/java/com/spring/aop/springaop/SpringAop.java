5093faa9bd0b351675de9fd19bb8a56f761c54a4
rkritchat <rkritchat@SCAN-IT-THBKK28>
2018-03-16 09:58:08 +0700

    Create after, around, afterThrowing, afterReturning

--- aop/src/main/java/com/spring/aop/springaop/SpringAop.java
+++ aop/src/main/java/com/spring/aop/springaop/SpringAop.java
@@ -5,4 +5,20 @@ public class SpringAop {
 	public void before() {
 		System.out.println("--->>Before run business logic");
 	}
+	
+	public void after() {
+		System.out.println("--->>After run business logic");
+	}
+	
+	public void around() {
+		System.out.println("---> Work around");
+	}
+	
+	public void afterThrowing(Throwable ex) {
+		System.out.println("--->After throwing" + ex);
+	}
+	
+	public void afterReturninig(Object resultObj) {
+		System.out.println("--->after returning" + resultObj);
+	}
 }
